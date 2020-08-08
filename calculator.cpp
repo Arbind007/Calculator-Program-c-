5addb21428527c6f2807869af5c82763a7ed81da
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2020-08-08 16:46:36 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int a,b,c;
+   scanf("%d",&a);
+   scanf("%d",&b);
+   
+   c = a + b;
+   printf("%d\n",c);
+   
+    c = a - b;
+   printf("%d\n",c);
+   
+    c = a * b;
+   printf("%d\n",c);
+   
+    c = a / b;
+   printf("%d\n",c);
+   
+    c = a % b;
+   printf("%d\n",c);
+   
+    
+
+    return 0;
+}
